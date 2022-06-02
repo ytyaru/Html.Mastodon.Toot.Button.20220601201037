32ccd191e9b6f4e318af1a48c342955ed93d9e6a
ytyaru <ytyaru@outlook.jp>
2022-06-02 10:37:14 +0900

    localStorageをsessionStorageに変更した。

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,5 +1,5 @@
 window.addEventListener('DOMContentLoaded', async (event) => {
-    console.log('DOMContentLoaded!!');
+    console.debug('DOMContentLoaded!!');
     const tooter = new Tooter('pawoo.net')
     const url = new URL(location.href)
     if (url.searchParams.has('code')) { // マストドンAPI oauth/authorize でリダイレクトされた場合
@@ -9,49 +9,49 @@ window.addEventListener('DOMContentLoaded', async (event) => {
         params.delete('code');
         history.replaceState('', '', url.pathname);
         // トークンを取得して有効であることを確認しトゥートする
-        const status = localStorage.getItem('status')
+        const status = sessionStorage.getItem('status')
         if (status) { document.getElementById('status').value = status; }
-        console.log('----- authorized -----')
-        console.log('client_id:', localStorage.getItem('client_id'))
-        console.log('client_secret:', localStorage.getItem('client_secret'))
-        console.log('認証コード', code)
-        // client_id, client_secretはLocalStorageに保存しておく必要がある
-        const json = await tooter.getToken(localStorage.getItem('client_id'), localStorage.getItem('client_secret'), code)
-        console.log(json)
-        console.log('access_token:', json.access_token)
-        localStorage.setItem('access_token', json.access_token);
+        console.debug('----- authorized -----')
+        console.debug('client_id:', sessionStorage.getItem('client_id'))
+        console.debug('client_secret:', sessionStorage.getItem('client_secret'))
+        console.debug('認証コード', code)
+        // client_id, client_secretはsessionStorageに保存しておく必要がある
+        const json = await tooter.getToken(sessionStorage.getItem('client_id'), sessionStorage.getItem('client_secret'), code)
+        console.debug(json)
+        console.debug('access_token:', json.access_token)
+        sessionStorage.setItem('access_token', json.access_token);
         const accessToken = json.access_token
         const v = await tooter.verify(accessToken)
-        console.log(v)
+        console.debug(v)
         const res = await tooter.toot(accessToken)
-        console.log(res)
+        console.debug(res)
         document.getElementById('res').value = JSON.stringify(res)
-        localStorage.removeItem('status')
-        console.log('----- 以上 -----')
+        sessionStorage.removeItem('status')
+        console.debug('----- 以上 -----')
     }
     document.getElementById('toot').addEventListener('click', async(event) => {
-        const access_token = localStorage.getItem('access_token')
+        const access_token = sessionStorage.getItem('access_token')
         if (access_token && tooter.verify(access_token)) {
-            console.log('既存のトークンが有効なため即座にトゥートします。');
+            console.debug('既存のトークンが有効なため即座にトゥートします。');
             const res = await tooter.toot(access_token)
-            console.log(res)
+            console.debug(res)
             document.getElementById('res').value = JSON.stringify(res)
         } else {
-            console.log('既存のトークンがないか無効のため、新しいアクセストークンを発行します。');
+            console.debug('既存のトークンがないか無効のため、新しいアクセストークンを発行します。');
             const app = await tooter.createApp()
-            localStorage.setItem('client_id', app.client_id);
-            localStorage.setItem('client_secret', app.client_secret);
-            localStorage.setItem('status', document.getElementById('status').value);
-            console.log(app)
-            console.log(app.client_id)
-            console.log(app.client_secret)
-            console.log(localStorage.getItem('client_id'))
-            console.log(localStorage.getItem('client_secret'))
+            sessionStorage.setItem('client_id', app.client_id);
+            sessionStorage.setItem('client_secret', app.client_secret);
+            sessionStorage.setItem('status', document.getElementById('status').value);
+            console.debug(app)
+            console.debug(app.client_id)
+            console.debug(app.client_secret)
+            console.debug(sessionStorage.getItem('client_id'))
+            console.debug(sessionStorage.getItem('client_secret'))
             tooter.authorize(app.client_id)
         }
     });
 });
 window.addEventListener('beforeunload', (event) => {
-    console.log('beforeunload!!');
+    console.debug('beforeunload!!');
 });
 
